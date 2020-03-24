d3998d7f1905d6983275980890f24cc63aa567e3
呜路塞 <12640449@qq.com>
2020-03-24 16:00:35 +0800

    变更

---
+++ src/main/java/net/wulusai/javastudy/alibaba/AliBase.java
@@ -0,0 +1,11 @@
+package net.wulusai.javastudy.alibaba;
+
+/**
+ * @author: wulusai
+ * @PackageName: net.wulusai.javastudy.alibaba
+ * @ClassName: AliBase
+ * @Description:
+ * @date: 2020/3/24 15:54
+ */
+public class AliBase {
+}
